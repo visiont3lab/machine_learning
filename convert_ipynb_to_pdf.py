8ad7b475640cce2853bf5bc35fcc77584d2ecf77
manuelrucci <rucci@localhost.com>
2019-12-14 17:03:57 +0100

    updated 12 december class

---
+++ convert_ipynb_to_pdf.py
@@ -0,0 +1,5 @@
+# Install the necessary packages into the virtual machine with:
+#apt-get install texlive texlive-xetex texlive-latex-extra pandoc
+#pip3 install pypandoc
+
+jupyter nbconvert --to PDF "../4-Datasets/04_scikit-learn-Dataset_PCA.ipynb"
